9eb4b4513bab1a321cd1534fd889c648d503cd69
Alex Gould <alexgould17@gmail.com>
2017-02-19 16:37:22 -0800

    Added unique data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 require 'random_data'
 
 50.times { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
-posts = Post.all
 
+upost = Post.find_or_create_by!(title: "Unique Post", body: "Unique test post.")
+posts = Post.all
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
+Comment.find_or_create_by!(post: upost, body: "Unique test comment.")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
