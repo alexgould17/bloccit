3b0f7a8bbba8090a3b35f15b4dec80b35edd5d89
Alex Gould <alexgould17@gmail.com>
2017-02-20 22:24:22 -0800

    Added spam filter.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.length.times do |i|
+      @posts[i].title = 'SPAM' if i%5 == 0
+    end
   end
 
   def show
