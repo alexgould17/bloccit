9ace664853138aa41da0d69b98d132a6a48a5770
Alex Gould <alexgould17@gmail.com>
2017-02-23 23:44:41 -0800

    Added Advertisements model & controller along with all associated routes/functionality.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'random_data'
 
+# Seed Posts
 50.times { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
 upost = Post.find_or_create_by!(title: "Unique Post", body: "Unique test post.")
@@ -7,6 +8,10 @@ posts = Post.all
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
 Comment.find_or_create_by!(post: upost, body: "Unique test comment.")
 
+# Seed Advertisements
+10.times { Advertisement.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: rand(69..420)) }
+
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Post.count} posts created."
+puts "#{Comment.count} comments created."
+puts "#{Advertisement.count} ads created."
