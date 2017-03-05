3aca235e8d7852981ea06b4eb24d21647033e740
Alex Gould <alexgould17@gmail.com>
2017-03-04 16:03:44 -0800

    Everything except the rspec tests for POST works. Ask Juke about those.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@ posts = Post.all
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
 Comment.find_or_create_by!(post: upost, body: "Unique test comment.")
 
+15.times { Question.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false) }
+
 puts "Seed finished"
+puts "#{Question.count} questions created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
