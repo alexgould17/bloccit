d4e13add4e0650a0623dc1969d3dff22e4104e99
Alex Gould <alexgould17@gmail.com>
2017-03-08 21:56:14 -0800

    Checkpoint 23 work: added user model.

--- Gemfile
+++ Gemfile
@@ -7,12 +7,12 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
-# Use sqlite3 as the database for Active Record (moved to dev/test)
-
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+# Use BCrypt for encrypting User passwords
+gem 'bcrypt'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307070038) do
+ActiveRecord::Schema.define(version: 20170309044313) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -61,6 +61,14 @@ ActiveRecord::Schema.define(version: 20170307070038) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
