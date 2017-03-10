5178cc544c67bb571f05540937fe580770ca5b06
Alex Gould <alexgould17@gmail.com>
2017-03-09 20:26:30 -0800

    Capitalize name function installed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   before_save { self.email.downcase! if email.present? }
+  # Capitalize first & last name before saving
+  before_save { self.name = self.name.split(' ').each{|s| s.capitalize!}.join(' ') }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
