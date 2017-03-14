e127c1baafcec4375f21c81ae8a62f685ca3fe98
Alex Gould <alexgould17@gmail.com>
2017-03-13 22:15:19 -0700

    Checkpoint 28 work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
-  
+
   before_save { self.email.downcase! if email.present? }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -9,4 +10,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 require 'random_data'
 5.times { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: RandomData.random_sentence) }
+admin = User.create!(name: "Alex Gould", email: "alexgould17@gmail.com", password: "123456", role: :admin)
+member = User.create!(name: "Test Member", email: "member@example.com", password: "123456")
 users = User.all
 15.times { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 topics = Topic.all
 50.times { Post.create!(user: users.sample, topic: topics.sample, title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 posts = Post.all
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
-User.first.update_attributes!(name: 'Alex Gould', email: 'alexgould17@gmail.com', password: '123456')
 
 puts "Seed finished"
 puts "#{User.count} users created"
