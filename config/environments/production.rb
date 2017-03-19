8f034bc8e23a26df1e110eb964814068b1364c20
Alex Gould <alexgould17@gmail.com>
2017-03-19 13:33:13 -0700

    mailer added.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer for favorites
+  config.action_mailer.default_url_options = { host: 'guarded-shelf-93839.herokuapp.com' }
 end
